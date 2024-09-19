24e2d21ddded3d84ed1cb6c096e3bff24257c690
Jwala Yadav <jwalayadav141@gmail.com>
2024-09-19 12:06:30 +0530

    Code Optimization

--- Entrata/src/test/java/baseTest/BaseClass.java
+++ Entrata/src/test/java/baseTest/BaseClass.java
@@ -2,27 +2,48 @@ package baseTest;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 	
 	public WebDriver driver;
 
 	@BeforeClass
-	public void setUp() {
-		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\test\\java\\Resource\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.get("https://www.amazon.in/");
+	public void setUp() {		
+		WebDriverManager.chromedriver().browserVersion("77.0.3865.40").setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized"); 
+		options.addArguments("enable-automation"); 
+		options.addArguments("--no-sandbox"); 
+		options.addArguments("--disable-infobars");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--disable-browser-side-navigation"); 
+		options.addArguments("--disable-gpu"); 
+		driver = new ChromeDriver(options); 
+		
+		// Launch URL 
+		driver.get("https://www.entrata.com/");
+		
+		// Maximize the window
 		driver.manage().window().maximize();
+		
+		//Set Implicit wait which will applicable for whole class
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		// Accept the cookies present on home page 
+		driver.findElement(By.xpath("//a[@id=\"cookie-accept\"]")).click();
 	}
 	
 	@AfterClass
 	public void tearDown() {
-		driver.close();
+		driver.quit();
 	}
 
 }
--- Entrata/src/test/java/entrara/RunTest.java
+++ Entrata/src/test/java/entrara/RunTest.java
@@ -1,36 +1,83 @@
 package entrara;
 
 import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import baseTest.BaseClass;
 
-public class RunTest{
-	public WebDriver driver;
+public class RunTest extends BaseClass{
+	// Define explicit wait
+	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+	
+	@Test(priority = 0, enabled = true)
+	public void products() {
+		// click on resident pay which appears after hover on product from menu bar
+		WebElement products = driver.findElement(By.xpath("//div[contains(text(),\"Products\")]"));
+		Actions action = new Actions(driver);
+		action.moveToElement(products).perform();
+		WebElement residentPay = driver.findElement(By.xpath("//*[@id=\"w-dropdown-list-7\"]/div/div[2]/div[1]/a[2]"));
+		residentPay.click();
+		WebElement header = driver.findElement(By.xpath("//h1[@class=\"product-name space superscript-filter\"]"));
+		String productHeader = header.getText();
+		// verifying the header is expected as actual or not with help oof assertEquals
+		Assert.assertEquals(productHeader, "ResidentPay");
 
-	@BeforeClass
-	public void setUp() {
-		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\src\\test\\java\\Resource\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.get("https://www.entrata.com/");
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.findElement(By.xpath("//a[@id=\"cookie-accept\"]")).click();
 	}
 	
-	@Test
+	@Test(priority = 1, enabled = true)
+	public void summit() throws InterruptedException {
+		WebElement summit = driver.findElement(By.xpath("(//a[contains(@href,\"summit\")])[1]"));
+		wait.until(ExpectedConditions.visibilityOf(summit));
+		summit.click();
+
+		// after click on summit, it's open another window, for that use window handling
+		// store parent window address for again back to main window
+		String parent = driver.getWindowHandle();
+		Set<String> window = driver.getWindowHandles();
+		Iterator<String> I = window.iterator();
+		while (I.hasNext()) {
+			String summitWindow = I.next();
+			if (!parent.equals(summitWindow)) {
+				driver.switchTo().window(summitWindow);
+			}
+		}
+
+		WebElement overview = driver.findElement(By.xpath("//a[contains(text(),\"Overview\")]"));
+		WebElement summitDate = driver.findElement(By.xpath("//div[@class=\"summit-dates\"]"));
+		WebElement registerNow = driver.findElement(By.xpath("(//a[@href=\"https://cvent.me/mByGQA\"])[3]"));
+		wait.until(ExpectedConditions.visibilityOf(overview));
+		// verifying the overview text is visible after open summit page
+		boolean view = overview.isDisplayed();
+		Assert.assertTrue(view);
+		// verifying the summit date is visible on summit page using assertTrue
+		boolean summitD = summitDate.isDisplayed();
+		Assert.assertTrue(summitD);
+
+		wait.until(ExpectedConditions.elementToBeClickable(registerNow));
+		registerNow.click();
+		WebElement registerNowHeader = driver.findElement(By.xpath("//div[contains(text(),\"Event Registration\")]"));
+		wait.until(ExpectedConditions.visibilityOf(registerNowHeader));
+		String registerNowHeaderText = registerNowHeader.getText();
+		Assert.assertEquals(registerNowHeaderText, "Event Registration");
+
+		driver.switchTo().window(parent);
+	}
+	
+	@Test(priority = 2, enabled = true)
 	public void WatchDemo() throws InterruptedException {
-		Thread.sleep(5000);
-		WebElement watchDemo = driver.findElement(By.xpath("/html/body/section[1]/div[14]/div[1]/nav/div[5]/a[1]"));
-		Thread.sleep(5000);
+		// open the url for watch demo and fill all details without click on submit
+		WebElement watchDemo = driver.findElement(By.xpath(
+				"//div[contains(text(),\"Watch Demo\")]/ancestor::div[contains(@class,\"nav-button\")]//a[contains(@href,\"go.entrata.com/watch-demo\")]"));
+		wait.until(ExpectedConditions.elementToBeClickable(watchDemo));
 		watchDemo.click();
 		WebElement firstName = driver.findElement(By.xpath("//input[@id=\"FirstName\"]"));
 		WebElement lastName = driver.findElement(By.xpath("//input[@id=\"LastName\"]"));
@@ -42,37 +89,15 @@ public class RunTest{
 		WebElement iAm = driver.findElement(By.xpath("//select[@id=\"demoRequest\"]"));
 		firstName.sendKeys("Jwala");
 		lastName.sendKeys("Yadav");
-		email.sendKeys("xyz@gmail.com");
+		email.sendKeys("jwalayadav@gmail.com");
 		companyName.sendKeys("Entrata");
 		phoneNumber.sendKeys("9456322156");
+		// select unit from dropdown
 		Select unit = new Select(unitCount);
 		unit.selectByVisibleText("1 - 10");
 		jobTitle.sendKeys("Software Quality Engineer");
-		Select iam=new Select(iAm);
+		Select iam = new Select(iAm);
 		iam.selectByVisibleText("a Resident");
-		
 	}
-	
-	@Test(enabled=false)
-	public void products() {
-		WebElement products=driver.findElement(By.xpath("//div[contains(text(),\"Products\")]"));
-		Actions action = new Actions(driver);
-		action.moveToElement(products).perform();
-		WebElement residentPay = driver.findElement(By.xpath("//*[@id=\"w-dropdown-list-7\"]/div/div[2]/div[1]/a[2]"));
-//		WebElement residentPay = driver.findElement(By.xpath("//div[@class=\"mega-nav desktop\"]//a[@href=\"/products/residentpay\"]"));
-		residentPay.click();
-		WebElement header=driver.findElement(By.xpath("//h1[@class=\"product-name space superscript-filter\"]"));
-		String productHeader = header.getText();
-		Assert.assertEquals(productHeader, "ResidentPay");
-		
-	}
-	@Test(enabled=false)
-	public void solutions() {
-		
-	}
-	
-	@AfterClass
-	public void tearDown() {
 
-	}
 }
